766f78d2a79af8dc503e1b4bf3df61f914309ded
Bartlomiej Kregielewski <bartlomiejkregielewski@gmail.com>
2021-04-05 15:26:24 +0200

    Project initial structure

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    /**
+     * Entry point of the application.
+     * @param args params passed to the application.
+     */
+    public static void main(String[] args) {
+
+    }
+}
