7e80a46d4982e9241b9a34a95a2910475613864c
Szymon Lepianka <slepianka@wp.pl>
2021-05-14 02:59:29 +0200

    getters, setters, drawing Firing and Intervention

--- src/main/java/entities/Intervention.java
+++ src/main/java/entities/Intervention.java
@@ -1,6 +1,12 @@
 package entities;
 
-public class Intervention extends Incident {
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.viewer.GeoPosition;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+public class Intervention extends Incident implements IDrawable {
 
     private final long duration;
     private final boolean willChangeIntoFiring;
@@ -12,7 +18,7 @@ public class Intervention extends Incident {
         super(latitude, longitude);
 
         // TODO Random parameters selection
-        this.duration = 0;
+        this.duration = 500;
         this.willChangeIntoFiring = false;
         this.timeToChange = -1;
     }
@@ -40,4 +46,30 @@ public class Intervention extends Incident {
         // TODO
     }
 
+    @Override
+    public void drawSelf(Graphics2D g, JXMapViewer mapViewer) {
+        var oldColor = g.getColor();
+
+        g.setColor(Color.ORANGE);
+
+        final var size = 10;
+        var point = mapViewer.convertGeoPositionToPoint(new GeoPosition(getLatitude(), getLongitude()));
+
+        var mark = new Ellipse2D.Double((int) (point.getX() - size / 2), (int) (point.getY() - size / 2), size, size);
+        g.fill(mark);
+
+        g.setColor(oldColor);
+    }
+
+    public Patrol getPatrolSolving() {
+        return patrolSolving;
+    }
+
+    public void setPatrolSolving(Patrol patrolSolving) {
+        this.patrolSolving = patrolSolving;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
 }
